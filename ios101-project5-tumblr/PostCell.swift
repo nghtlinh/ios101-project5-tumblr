a9d95a22d2236817c05c26225f5bbe22562eccc8
Monica Nguyen-Hoang <nguyenhl@dickinson.edu>
2024-03-25 22:00:29 -0400

    Fectched the summary of post

---
+++ ios101-project5-tumblr/PostCell.swift
@@ -0,0 +1,8 @@
+//
+//  PostCell.swift
+//  ios101-project5-tumblr
+//
+//  Created by Linh on 3/25/24.
+//
+
+import Foundation
--- ios101-project5-tumblr/ViewController.swift
+++ ios101-project5-tumblr/ViewController.swift
@@ -6,11 +6,35 @@
 import UIKit
 import Nuke
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return posts.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        // Create, configure, and return a table view cell for the given row (i.e., `indexPath.row`)
+        
+        // Create the cell
+        let cell = UITableViewCell()
+        
+        // Get the movie-associated table view row
+        let post = posts[indexPath.row]
+        
+        // Configure the cell (i.e., update UI elements like labels, image views, etc.)
+        cell.textLabel?.text = post.summary
+        
+        // Return the cell for use in the respective table view row
+        return cell
+    }
+    
 
-
+    @IBOutlet weak var tableView: UITableView!
+    private var posts: [Post] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableView.dataSource = self
 
         
         fetchPosts()
@@ -48,6 +72,9 @@ class ViewController: UIViewController {
                     for post in posts {
                         print("🍏 Summary: \(post.summary)")
                     }
+                    
+                    self?.posts = posts
+                    self?.tableView.reloadData()
                 }
 
             } catch {
